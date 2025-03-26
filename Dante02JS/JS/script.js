505c2835fcc4647282614cdee22c961623f13618
DanteCatini <163876885+DanteCatini@users.noreply.github.com>
2025-03-26 09:34:43 -0300

    Delete Dante02JS directory

--- Dante02JS/JS/script.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("tareas")
-const button = document.querySelector("button")
-
-function saludo() {
-    alert(input.value)
-}
-
-button.addEventListener("click", saludo)
