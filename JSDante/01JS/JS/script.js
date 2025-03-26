765a23931bb8750ec586b97979ef7988a2c6cbee
DanteCatini <163876885+DanteCatini@users.noreply.github.com>
2025-03-26 09:37:56 -0300

    Add files via upload

---
+++ JSDante/01JS/JS/script.js
@@ -0,0 +1,11 @@
+document.getElementById("azul").addEventListener("click", function(){
+    document.getElementById("cuadrado").style.backgroundColor = "blue"
+});
+
+document.getElementById("rojo").addEventListener("click", function(){
+    document.getElementById("cuadrado").style.backgroundColor = "red"
+});
+
+document.getElementById("verde").addEventListener("click", function(){
+    document.getElementById("cuadrado").style.backgroundColor = "green"
+});
