1bca60f87e3deaa00b15a6785f57ef9fdce22517
Micaolmos <162600985+Micaolmos@users.noreply.github.com>
2025-03-27 09:39:45 -0300

    Add files via upload

--- MicaOlmos/LISTADETAREAS/script.js
+++ MicaOlmos/LISTADETAREAS/script.js
@@ -1,18 +1,27 @@
 function agregarTarea() {
     let input = document.getElementById("tareaInput");
-    let tareaTexto = input.value.trim();
+    let tareaTexto = input.value.trim(); //elimina espacios en blanco 
     
-    if (tareaTexto === "") return;
+    if (tareaTexto === "") return; //si el input estar vacio no agrega nada 
     
-    let li = document.createElement("li");
-    let botonEliminar = document.createElement("button");
+    let lista = document.getElementById("listaTareas"); //obtiene la lista de tareas 
+    let nuevaTarea = document.createElement("li"); //crea un elemento li par ala lista de tareas
+    nuevaTarea.textContent = tareaTexto;
     
-    li.textContent = tareaTexto;
-    botonEliminar.textContent = "Eliminar";
-    botonEliminar.onclick = function() { li.remove(); };
+    let botonEliminar = document.createElement("button"); //crea un boton para eliminar cada tarea 
+    botonEliminar.textContent = "Eliminar Tarea";
+
+    botonEliminar.onclick = function() { //
+        lista.removeChild(nuevaTarea);
+    };
     
-    li.appendChild(botonEliminar);
-    document.getElementById("listaTareas").appendChild(li);
-    
-    input.value = "";
+    nuevaTarea.appendChild(botonEliminar); //agrega el boton al elemento de la tarea 
+    lista.appendChild(nuevaTarea); //agrega la tarea nueva a la lista 
+    input.value = ""; //limpia el imput 
 }
+
+document.getElementById("tareaInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        agregarTarea();
+    }
+});
