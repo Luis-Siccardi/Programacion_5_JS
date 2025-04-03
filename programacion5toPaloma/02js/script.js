b41282a403e7662573f1ae9d7e81cddbcdc10c7e
PalomaCarranza <164367090+PalomaCarranza@users.noreply.github.com>
2025-04-03 08:34:59 -0300

    Add files via upload

--- programacion5toPaloma/02js/script.js
+++ programacion5toPaloma/02js/script.js
@@ -1,5 +1,5 @@
 document.getElementById("agregarBtn").onclick = () => {
-    let input = document.getElementById("tareaInput"), texto = input.value.trim(); // Obtiene el input y su contenido sin espacios extra
+    let input = document.getElementById("tareaInput"), texto = input.value.trim(); // Obtiene el input y su contenido
     if (!texto) return; // Si el input está vacío, no hace nada
     
     // Agrega la tarea a la lista con un botón para eliminarla
